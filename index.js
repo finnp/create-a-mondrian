53615913b163f5969371685d677fe95673353358
Finn Pauls <derfinn@gmail.com>
2018-11-18 20:43:57 +0100

    route everythin to the app

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.use(require('./stores/clicks'))
 
-app.route('/', require('./views/main'))
-app.route('/*', require('./views/404'))
+app.route('/*', require('./views/main'))
 
 module.exports = app.mount('body')
