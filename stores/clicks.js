b95a3f04e98839fdb100f94b7e5f5c8ada1eb433
Finn Pauls <derfinn@gmail.com>
2018-11-18 15:37:25 +0100

    This is starting to be fun

--- stores/clicks.js
+++ stores/clicks.js
@@ -1,23 +1,63 @@
 module.exports = store
 
 function store (state, emitter) {
+  const width = state.width = 700
+  const height = state.height = 700
+  const lineWidth = state.lineWidth = 20
+
+  state.rects = [{
+    x: 0,
+    y: 0,
+    height: height,
+    width: width
+  }]
+
   state.verticals = [
     {x: 0, y: 0, length: 700},
-    {x: 700-20, y: 0, length: 700},
+    {x: 700, y: 0, length: 700},
   ]
   state.horizontals = [
     {x: 0, y: 0, length: 700},
-    {x: 0, y: 700-20, length: 700},
+    {x: 0, y: 700, length: 700},
   ]
   state.visible = false
   state.posY = 0
   state.length = 700
 
   emitter.on('DOMContentLoaded', function () {
+    emitter.on('rect:hover', function (rect) {
+      state.currentRect = rect
+      emitter.emit(state.events.RENDER)
+    })
     emitter.on('line:click', function () {
       if (!state.isVertical) {
+        const x = state.pos
+        const y = state.start
+        const possibleRects = state.rects.filter(rect => rect.y === y)
+        const ignoredRects = state.rects.filter(rect => rect.y !== y)
+
+        const rectsLeft = possibleRects.filter(rect => rect.x <= x)
+        const rectsRight = possibleRects.filter(rect => rect.x > x)
+
+        const rect = rectsLeft.sort((a,b) => a.x - b.x).pop()
+        state.rects = ignoredRects
+          .concat(rectsLeft)
+          .concat(rectsRight)
+          .concat(splitRectVertical(rect, state.pos))
         state.verticals.push({x: state.pos, y: state.start, length: state.length})
       } else {
+        const y = state.pos
+        const x = state.start
+        const possibleRects = state.rects.filter(rect => rect.x === x)
+        const ignoredRects = state.rects.filter(rect => rect.x !== x)
+
+        const rectsUp = possibleRects.filter(rect => rect.y <= y)
+        const rectsDown = possibleRects.filter(rect => rect.y > y)
+        const rect = rectsUp.sort((a,b) => a.y - b.y).pop()
+        state.rects = ignoredRects
+          .concat(rectsUp)
+          .concat(rectsDown)
+          .concat(splitRectHorizontal(rect, state.pos))
         state.horizontals.push({x: state.start, y: state.pos, length: state.length})
       }
       state.visible = false
@@ -26,20 +66,23 @@ function store (state, emitter) {
     emitter.on('line:hoverVertical', function ({x,y}) {
       // positioning horizontal line
       state.isVertical = true
+      state.currentRect = null
       state.pos = y
       state.start = x
       state.length = state.verticals
-        .filter(v => v.y <= y && y <= v.y + v.length && v.x >= x)
+        .filter(v => v.y <= y && y <= v.y + v.length && v.x > x)
         .sort((a, b) => a.x - b.x)[0].x - x
       state.visible = true
       emitter.emit(state.events.RENDER)
     })
     emitter.on('line:hoverHorizontal', function ({x,y}) {
+      // positioning vertical line
       state.isVertical = false
+      state.currentRect = null
       state.pos = x
       state.start = y
       const endPos = state.horizontals
-        .filter(h => h.x <= x && x <= h.x + h.length && h.y >= y)
+        .filter(h => h.x <= x && x <= h.x + h.length && h.y > y)
         .sort((a, b) => a.y - b.y)[0].y - y
       state.length = endPos
       state.visible = true
@@ -51,4 +94,20 @@ function store (state, emitter) {
       emitter.emit(state.events.RENDER)
     })
   })
+}
+
+function splitRectVertical (rect, x) {
+  const posX = x - rect.x
+  return [
+    {x: rect.x, y: rect.y, width: posX, height: rect.height},
+    {x: rect.x + posX, y: rect.y, width: rect.width - posX, height: rect.height}
+  ]
+}
+
+function splitRectHorizontal (rect, y) {
+  const posY = y - rect.y
+  return [
+    {x: rect.x, y: rect.y, height: y, width: rect.width},
+    {x: rect.x, y: rect.y + posY, height: rect.height - posY, width: rect.width}
+  ]
 }
--- views/main.js
+++ views/main.js
@@ -1,17 +1,16 @@
 var html = require('choo/html')
 
-var TITLE = 'create-a-mondrian - main'
+var TITLE = 'create a mondrian'
 
 module.exports = view
 
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
 
+  // TODO: replace with state.width etc.
   const width = 700
   const height = width
-
   const lineWidth = 20
-
 
   return html`
     <body class="code lh-copy">
@@ -20,14 +19,20 @@ function view (state, emit) {
    width="${width}" height="${height}"
    xmlns="http://www.w3.org/2000/svg">
 
+   ${
+     state.rects.map((rect) => {
+       if (rect === state.currentRect) return renderColorPicker(rect)
+       return html`<rect onmousemove=${onHoverRectangle.bind(null, rect)} x=${rect.x} y=${rect.y} height=${rect.height} width=${rect.width}  fill="white"/>`
+     })
+   }
 
-   ${state.visible && html`<rect x="${state.isVertical ? state.start : state.pos}" y="${state.isVertical ? state.pos : state.start}" width="${state.isVertical ? state.length : lineWidth}" height="${state.isVertical ? lineWidth : state.length}" fill="grey" />`}
+   ${state.visible && html`<rect x="${state.isVertical ? state.start : state.pos}" y="${state.isVertical ? state.pos : state.start}" width="${state.isVertical ? state.length : lineWidth}" height="${state.isVertical ? lineWidth : state.length}" fill="rgba(0,0,0,0.3)" />`}
 
-   ${state.verticals.map(({x,y,length}) =>
-    html`<rect style="cursor: pointer;" onclick=${onLineClick} onmouseout=${onLineOut} onmousemove=${onHoverVertical} x="${x}" y="${y}" width="${lineWidth}" height="${length}" fill="black" />`
+   ${rectsToVerticals(state.rects).map(({x,y,length}) =>
+    html`<line style="cursor: pointer;" onclick=${onLineClick} onmouseout=${onLineOut} onmousemove=${onHoverVertical.bind(null, x)} x1="${x}" y1="${y}" x2="${x}" y2="${y+length}" style="stroke:black;stroke-width:20;" />`
   )}
-   ${state.horizontals.map(({x,y,length}) =>
-    html`<rect style="cursor: pointer;" onclick=${onLineClick} onmouseout=${onLineOut} onmousemove=${onHoverHorizontal} x="${x}" y="${y}" width="${length}" height="${lineWidth}" fill="black" />`
+   ${rectsToHorizontals(state.rects).map(({x,y,length}) =>
+    html`<line style="cursor: pointer;" onclick=${onLineClick} onmouseout=${onLineOut} onmousemove=${onHoverHorizontal.bind(null,y)} x1="${x}" y1="${y}" x2="${x+length}" y2="${y}" style="stroke:black;stroke-width:20;" />`
   )}
 
    </svg>
@@ -38,18 +43,48 @@ function view (state, emit) {
     emit('line:click')
   }
 
-  function onHoverVertical (e) {
-    const x = e.offsetX
+  function onHoverVertical (x, e) {
     const y = e.offsetY
     emit('line:hoverVertical', {x,y})
   }
-  function onHoverHorizontal (e) {
+  function onHoverHorizontal (y, e) {
     const x = e.offsetX
-    const y = e.offsetY
     emit('line:hoverHorizontal', {x,y})
+  }
+
+  function onHoverRectangle (rect) {
+    emit('rect:hover', rect)
   }
 
   function onLineOut (e) {
     emit('line:out')
+  }
+
+  function rectsToVerticals (rects) {
+    return rects
+      .map(rect => ([
+        {x: rect.x, y: rect.y, length: rect.height},
+        {x: rect.x + rect.width, y: rect.y, length: rect.height},
+      ]))
+      .reduce((a, b) => a.concat(b))
+  }
+
+  function rectsToHorizontals (rects) {
+    return rects
+      .map(rect => ([
+        {x: rect.x, y: rect.y, length: rect.width},
+        {x: rect.x, y: rect.y + rect.height, length: rect.width},
+      ]))
+      .reduce((a, b) => a.concat(b))
+  }
+
+  function renderColorPicker (rect) {
+    return html`
+     <g>
+       <rect x=${rect.x} y=${rect.y} height=${rect.height} width=${rect.width/3}  fill="red"/>
+       <rect x=${rect.x + rect.width/3} y=${rect.y} height=${rect.height} width=${rect.width/3}  fill="yellow"/>
+       <rect x=${rect.x + 2*rect.width/3} y=${rect.y} height=${rect.height} width=${rect.width/3}  fill="blue"/>
+     </g>
+    `
   }
 }
